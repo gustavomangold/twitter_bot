ab958081b7b92325588d62d0cdcc4fd08dbb73bf
Gustavo Mangold <104363966+gustavomangold@users.noreply.github.com>
2023-09-11 15:54:03 -0300

    Update main_script.py

--- main_script.py
+++ main_script.py
@@ -1,15 +1,14 @@
 import twitterbot as tb
 import secrets, sys
 
+# the command must be called with python3 main_script.py 'tweet_to_be_posted'
+# the argument is the tweet
 # fetches the hashtag from command line argument
 argument = sys.argv[1]
-# fetches the credentials dictionary
-# using get_credentials function
-credentials = secrets.get_credentials()
-# initialize the bot with your credentials
-bot = tb.Twitterbot('ja_pode_almosar', 'senhaforte21')
-# logging in
+
+# initialize the bot with your username/email or password
+bot = tb.Twitterbot('email_or_username-string', 'password-string')
 bot.login()
-# calling like_retweet function
-bot.post_tweets(argument)
+# calling post_tweet function
+bot.post_tweet(argument)
 
